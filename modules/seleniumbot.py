23170194e84618b6c2eb19b9a43dd3973821b259
Tanujairam <tanujairam.v@gmail.com>
2025-04-06 16:19:52 +0530

    Update seleniumbot.py

--- modules/seleniumbot.py
+++ modules/seleniumbot.py
@@ -1,138 +1,200 @@
-import logging
-import random
-import re
-import time
-import requests
+""" author: feezyhendrix
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import Select
+    main function botcore
+ """
+
+from time import sleep
+from random import randint
 
 import modules.config as config
+# importing generated info
 import modules.generateaccountinformation as accnt
 from modules.storeusername import store
+# from .activate_account import get_activation_url
+# library import
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys  # and Krates
+import requests
+import re
+import logging
+# from fake_useragent import UserAgent
 
+# from pymailutils import Imap
 
-logging.basicConfig(level=logging.INFO)
-
-class AccountCreator:
+class AccountCreator():
+    account_created = 0
     def __init__(self, use_custom_proxy, use_local_ip_address):
+        self.sockets = []
         self.use_custom_proxy = use_custom_proxy
         self.use_local_ip_address = use_local_ip_address
         self.url = 'https://www.instagram.com/accounts/emailsignup/'
-        self.sockets = self.__collect_sockets()
+        self.__collect_sockets()
+
 
     def __collect_sockets(self):
         r = requests.get("https://www.sslproxies.org/")
-        matches = re.findall(r"<td>\d+\.\d+\.\d+\.\d+</td><td>\d+</td>", r.text)
-        return [m.replace("<td>", "").replace("</td>", "").replace(":", ":") for m in matches]
+        matches = re.findall(r"<td>\d+.\d+.\d+.\d+</td><td>\d+</td>", r.text)
+        revised_list = [m1.replace("<td>", "") for m1 in matches]
+        for socket_str in revised_list:
+            self.sockets.append(socket_str[:-5].replace("</td>", ":"))
 
     def createaccount(self, proxy=None):
         chrome_options = webdriver.ChromeOptions()
+        if proxy != None:
+            chrome_options.add_argument('--proxy-server=%s' % proxy)
+
+        # chrome_options.add_argument('headless')
+        # ua = UserAgent()
+        # user_agent = ua.random
+        chrome_options.add_argument('--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36"')
+        # chrome_options.add_argument("--incognito")
         chrome_options.add_argument('window-size=1200x600')
-        chrome_options.add_argument(
-            '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36'
-        )
-        if proxy:
-            chrome_options.add_argument(f'--proxy-server={proxy}')
-
-        driver = webdriver.Chrome(
-            service=Service(config.Config['chromedriver_path']),
-            options=chrome_options
-        )
-
-        logging.info("Opening Browser")
+        driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=config.Config['chromedriver_path'])
+        print('Opening Browser')
         driver.get(self.url)
-        time.sleep(5)
 
+        print('Browser Opened')
+        sleep(5)
+
+
+
+
+        action_chains = ActionChains(driver)
+        sleep(5)
         account_info = accnt.new_account()
-        logging.info(f"Email: {account_info['email']}")
-        logging.info(f"Gender: {account_info['gender']}")
-        logging.info(f"Url generated: {account_info['url']}")
-        logging.info(f"Birthday: {account_info['birthday']}")
-        logging.info(f"Username: {account_info['username']}")
 
-        driver.find_element(By.NAME, "emailOrPhone").send_keys(account_info["email"])
-        logging.info("Filling email field")
+        # fill the email value
+        print('Filling email field')
+        email_field = driver.find_element_by_name('emailOrPhone')
+        print(email_field)
+        sleep(1)
+        action_chains.move_to_element(email_field)
+        print(account_info["email"])
+        email_field.send_keys(str(account_info["email"]))
 
-        driver.find_element(By.NAME, "fullName").send_keys(account_info["name"])
-        logging.info("Filling fullname field")
+        sleep(2)
 
-        driver.find_element(By.NAME, "username").send_keys(account_info["username"])
-        logging.info("Filling username field")
+        # fill the fullname value
+        print('Filling fullname field')
+        fullname_field = driver.find_element_by_name('fullName')
+        action_chains.move_to_element(fullname_field)
+        fullname_field.send_keys(account_info["name"])
 
-        driver.find_element(By.NAME, "password").send_keys(account_info["password"])
-        logging.info("Filling password field")
+        sleep(2)
 
-        time.sleep(2)
+        # fill username value
+        print('Filling username field')
+        username_field = driver.find_element_by_name('username')
+        action_chains.move_to_element(username_field)
+        username_field.send_keys(account_info["username"])
 
+        sleep(2)
+
+        # fill password value
+        print('Filling password field')
+        password_field = driver.find_element_by_name('password')
+        action_chains.move_to_element(password_field)
+        passW = account_info["password"]
+        print(passW)
+        password_field.send_keys(str(passW))
+        sleep(1)
+
+        sleep(2)
+
+        submit = driver.find_element_by_xpath(
+            '//*[@id="react-root"]/section/main/div/div/div[1]/div/form/div[7]/div/button')
+
+        action_chains.move_to_element(submit)
+
+        sleep(2)
+        submit.click()
+
+        sleep(3)
         try:
-            signup_button = driver.find_element(By.XPATH, '//button[@type="submit"]')
-            signup_button.click()
-            logging.info("Clicking signup button")
-        except Exception as e:
-            logging.warning(f"[ERROR] Could not click signup button: {e}")
 
-        time.sleep(5)
+            month_button = driver.find_element_by_xpath( '//*[@id="react-root"]/section/main/div/div/div[1]/div/div[4]/div/div/span/span[1]/select')
+            month_button.click()
+            month_button.send_keys(account_info["birthday"].split(" ")[0])
+            sleep(1)
+            day_button = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div[1]/div/div[4]/div/div/span/span[2]/select')
+            day_button.click()
+            day_button.send_keys(account_info["birthday"].split[" "][1][:-1])
+            sleep(1)
+            year_button = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div[1]/div/div[4]/div/div/span/span[3]/select')
+            year_button.click()
+            year_button.send_keys(account_info["birthday"].split[" "][2])
 
-        try:
-            month = Select(driver.find_element(By.XPATH, '//select[@title="Month:"]'))
-            day = Select(driver.find_element(By.XPATH, '//select[@title="Day:"]'))
-            year = Select(driver.find_element(By.XPATH, '//select[@title="Year:"]'))
-
-            birthday_parts = account_info["birthday"].split(" ")
-            month.select_by_visible_text(birthday_parts[0])
-            day.select_by_visible_text(birthday_parts[1][:-1])
-            year.select_by_visible_text(birthday_parts[2])
-
-            logging.info("Filling birthday details")
-
-            next_button = driver.find_element(By.XPATH, '//button[text()="Next"]')
+            sleep(2)
+            next_button = driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/div/div[1]/div/div[6]/button')
             next_button.click()
 
-        except Exception as e:
-            logging.warning(f"[WARNING] Skipping birthday selection: {e}")
+        except Exception as e :
+            pass
 
-        # Ask user to manually enter the confirmation code
-        logging.info("[INFO] Waiting for confirmation code...")
-        user_email = input("Enter your email used for signup: ").strip()
-        confirmation_code = input("Enter the confirmation code received: ").strip()
 
-        # You can automate entry here if you know the input field name
-        # For now, we just wait to keep the session alive
-        time.sleep(10)
-
+        sleep(4)
+        # After the first fill save the account account_info
         store(account_info)
-        driver.quit()
+        
+        """
+            Currently buggy code.
+        """
+        # Activate the account
+        # confirm_url = get_activation_url(account_info['email'])
+        # logging.info("The confirm url is {}".format(confirm_url))
+        # driver.get(confirm_url)
+
+        driver.close()
 
     def creation_config(self):
         try:
-            if self.use_local_ip_address:
-                for _ in range(config.Config['amount_of_account']):
-                    try:
-                        self.createaccount()
-                    except Exception as e:
-                        logging.error(f"Error with local IP: {e}")
-            elif self.use_custom_proxy:
-                with open(config.Config['proxy_file_path'], 'r') as file:
-                    for proxy in file.read().splitlines():
-                        for _ in range(config.Config['amount_per_proxy']):
+            if self.use_local_ip_address == False:
+                if self.use_custom_proxy == False:
+                    for i in range(0, config.Config['amount_of_account']):
+                        if len(self.sockets) > 0:
+                            current_socket = self.sockets.pop(0)
                             try:
-                                self.createaccount(proxy)
+                                self.createaccount(current_socket)
                             except Exception as e:
-                                logging.warning(f"Error with custom proxy {proxy}: {e}")
+                                print('Error!, Trying another Proxy {}'.format(current_socket))
+                                self.createaccount(current_socket)
+
+                else:
+                    with open(config.Config['proxy_file_path'], 'r') as file:
+                        content = file.read().splitlines()
+                        for proxy in content:
+                            amount_per_proxy = config.Config['amount_per_proxy']
+
+                            if amount_per_proxy != 0:
+                                print("Creating {} amount of users for this proxy".format(amount_per_proxy))
+                                for i in range(0, amount_per_proxy):
+                                    try:
+                                        self.createaccount(proxy)
+
+                                    except Exception as e:
+                                        print("An error has occured" + e)
+
+                            else:
+                                random_number = randint(1, 20)
+                                print("Creating {} amount of users for this proxy".format(random_number))
+                                for i in range(0, random_number):
+                                    try:
+                                        self.createaccount(proxy)
+                                    except Exception as e:
+                                        print(e)
             else:
-                for _ in range(config.Config['amount_of_account']):
-                    if self.sockets:
-                        proxy = self.sockets.pop(0)
-                        try:
-                            self.createaccount(proxy)
-                        except Exception as e:
-                            logging.warning(f"Error with socket proxy {proxy}: {e}")
+                for i in range(0, config.Config['amount_of_account']):
+                            try:
+                                self.createaccount()
+                            except Exception as e:
+                                print('Error!, Check its possible your ip might be banned')
+                                self.createaccount()
+
+
         except Exception as e:
-            logging.error(f"General error: {e}")
+            print(e)
+
 
 def runbot():
     account = AccountCreator(config.Config['use_custom_proxy'], config.Config['use_local_ip_address'])
